a0d7b3cb954675ef51aca453d32104f5a642e71d
JD Gonzales <jd_gonzales@icloud.com>
2016-09-20 20:58:04 -0500

    Finished scroll CSS transition on selling points.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,32 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
-    var revealPoints = function(pointIndex) { 
-            points[pointIndex].style.opacity = 1;
-            points[pointIndex].style.transform = "scaleX(1) translateY(0)";
-            points[pointIndex].style.msTransform = "scaleX(1) translateY(0)";
-            points[pointIndex].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function(index) { 
+            pointsArray[index].style.opacity = 1;
+            pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+            pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+            pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
-    for(var i = 0; i < points.length; i++) {
-        revealPoints(i)
+    for(var i = 0; i < pointsArray.length; i++) {
+        revealPoint(i)
     }
 };
 
-animatePoints();
+window.onload = function() {
+
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event){
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
+
+
